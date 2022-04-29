363ce9507901a4e77015abdd470e6d0436186aeb
baco0929 <97611522+baco0929@users.noreply.github.com>
2022-04-30 00:23:58 +0900

    Add files via upload

---
+++ test/src/test/main.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:oracle:thin:@Ezenac1_high?TNS_ADMIN=/C:\\Users\\JSW\\Documents/Wallet_Ezenac1";
+		String userid = "admin";
+		String pwd = "Ez1234567890";
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			System.out.println("드라이버 로딩 성공");
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("데이터베이스 연결 준비");
+			Connection con = DriverManager.getConnection(url, userid, pwd);
+			if(con != null) {
+				System.out.println("데이터베이스 연결 성공");
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
